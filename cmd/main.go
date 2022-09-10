fb5d4f0a26016a9e45ab0d230169a775a1600c0c
Sancene <minefattoproject@gmail.com>
2022-09-10 16:55:26 +0300

    service and repo dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,17 @@ import (
 
 	todo "github.com/Sancene/go-rest-learn"
 	"github.com/Sancene/go-rest-learn/pkg/handler"
+	"github.com/Sancene/go-rest-learn/pkg/repository"
+	"github.com/Sancene/go-rest-learn/pkg/service"
 )
 
 func main() {
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	handlers := new(handler.Handler)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error has occured while running http server: %s", err.Error())
 	}
---
+++ pkg/service/service.go
@@ -0,0 +1,22 @@
+package service
+
+import "github.com/Sancene/go-rest-learn/pkg/repository"
+
+type Authorization interface {
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Service struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewService(repository *repository.Repository) *Service {
+	return &Service{}
+}
